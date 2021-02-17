cfca8a0cb411b322ea267c899729806b0ba4ada4
Silke Blansjaar <silkeblansjaar@gmail.com>
2021-02-17 13:37:46 +0000

    Installed use-sound and added audio to each key

--- src/Key.js
+++ src/Key.js
@@ -1,23 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import useSound from "use-sound";
 import "./Key.css"
+import mp3_file from "./notes/sprite.mp3"
 
 export default function Key(props) {
+  const [play, { stop }] = useSound(mp3_file, {
+    sprite: {
+      c1: [0, 1000],
+      d1f: [2000, 1000],
+      d1: [4000, 1000],
+      e1f: [6000, 1000],
+      e1: [8000, 1000],
+      f1: [10100, 1000],
+      g1f: [12000, 1000],
+      g1: [14000, 1000],
+      a1f: [16000, 1000],
+      a1: [18000, 1000],
+      b1f: [20000, 1000],
+      b1: [22000, 1000],
+      c2: [24000, 1000],
+    }
+  });
+
+
   let keyClassName = "Key";
   if (props.note.length > 2) {
     keyClassName += " flat";
   }
-
+  
   function handleMouseDown(event) {
     let key = event.target;
     key.classList.add("pressed");
+    play({id: props.note});
   }
 
   function handleMouseUp(event) {
     let key = event.target;
     key.classList.remove("pressed");
+    stop();
   }
-
+  
   return (
-    <div className={keyClassName} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseOut={handleMouseUp}></div>
+    <div className={keyClassName} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseOut={handleMouseUp}>
+    </div>
   )
 }
--- src/Piano.js
+++ src/Piano.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Key from "./Key";
 import "./Piano.css";
+import mp3_file from "./notes/a1.mp3";
+import useSound from "use-sound";
 
 export default function Piano() {
   const notes = ["c1", "d1f", "d1", "e1f", "e1", "f1", "g1f", "g1", "a1f", "a1", "b1f", "b1", "c2"];
-  const keys = notes.map((note, index) => <Key note={note} key={index} />)
+  const keys = notes.map((note, index) => <Key note={note} key={index} index={index} />)
+
+  const [play] = useSound(mp3_file);
 
   return (
     <div className="Piano">
-      <div className="piano-div">
-        {keys}
-      </div>
+      {keys}
     </div>
   )
   
