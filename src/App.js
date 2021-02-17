0be14f8f7f2fb9550fa17620f0a5584fb752b166
Silke Blansjaar <silkeblansjaar@gmail.com>
2021-02-17 13:44:30 +0000

    Added link to GitHub code in footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
     <div className="App">
       <h1>It's a piano!</h1>
       <Piano />
+      <footer><a href="https://github.com/silkeblan/piano-app" target="_blank">Open source code</a> by Silke Blansjaar</footer>
     </div>
   );
 }
